f42d2a2331a30b0a1703dd234fd15c854fb5f473
etwas <etwas@example.com>
2024-12-26 17:31:45 +0900

    ✨ 簡単なコマンドを追加

--- src/main/java/com/etw4s/twitchchatlink/client/TwitchChatLinkClient.java
+++ src/main/java/com/etw4s/twitchchatlink/client/TwitchChatLinkClient.java
@@ -1,10 +1,13 @@
 package com.etw4s.twitchchatlink.client;
 
+import com.etw4s.twitchchatlink.command.TwitchCommand;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class TwitchChatLinkClient implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
+    ClientCommandRegistrationCallback.EVENT.register(TwitchCommand::register);
   }
 }
---
+++ src/main/java/com/etw4s/twitchchatlink/command/TwitchCommand.java
@@ -0,0 +1,25 @@
+package com.etw4s.twitchchatlink.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.text.Text;
+
+public class TwitchCommand {
+
+  public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher,
+      CommandRegistryAccess access) {
+    dispatcher.register(ClientCommandManager.literal("twitch")
+        .then(ClientCommandManager.literal("connect")
+            .then(ClientCommandManager.argument("login", StringArgumentType.word())
+                .executes(TwitchCommand::connectHandler))));
+  }
+
+  private static int connectHandler(CommandContext<FabricClientCommandSource> context) {
+    context.getSource().sendFeedback(Text.literal("success!!"));
+    return 1;
+  }
+}
