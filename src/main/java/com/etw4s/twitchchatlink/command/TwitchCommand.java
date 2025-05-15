48badada9736a6313058134468c8872a800a8eeb
etwasttv <147773984+etwasttv@users.noreply.github.com>
2025-05-16 00:39:47 +0900

    :recycle: Refactor searchHandler and getSearchResultText methods for improved readability and structure

--- src/main/java/com/etw4s/twitchchatlink/command/TwitchCommand.java
+++ src/main/java/com/etw4s/twitchchatlink/command/TwitchCommand.java
@@ -2,6 +2,7 @@ package com.etw4s.twitchchatlink.command;
 
 import com.etw4s.twitchchatlink.TwitchChatLinkConfig;
 import com.etw4s.twitchchatlink.model.TwitchChannel;
+import com.etw4s.twitchchatlink.twitch.SearchChannelsResult;
 import com.etw4s.twitchchatlink.twitch.TwitchApi;
 import com.etw4s.twitchchatlink.twitch.TwitchApiException;
 import com.etw4s.twitchchatlink.twitch.auth.AuthManager;
@@ -16,6 +17,7 @@ import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.CommandSource;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -65,74 +67,79 @@ public class TwitchCommand {
         return;
       }
 
-      var response = Text.empty();
-      response.append(
-          Text.literal(query + "の検索結果 ==========\n")
-              .setStyle(Style.EMPTY
-                  .withBold(true)
-                  .withColor(Formatting.GOLD)));
-      var itr = result.channels().iterator();
-      while (itr.hasNext()) {
-        var data = itr.next();
-        var connect = Text.empty();
-        connect.append(Text.literal("["));
-        connect.append(Text.literal("接続")
-            .setStyle(Style.EMPTY
-                .withColor(Formatting.DARK_GREEN)
-                .withBold(true)));
-        connect.append(Text.literal("]"));
-        connect.setStyle(Style.EMPTY
-            .withHoverEvent(
-                new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                    Text.empty()
-                        .append(Text.literal("クリックして"))
-                        .append(Text.literal(data.displayName())
-                            .setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA).withItalic(true)))
-                        .append(Text.literal("のチャットに接続する"))))
-            .withClickEvent(
-                new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/twitch connect " + data.login())));
-        response.append(connect);
-        response.append(" ");
-        var channelInfo = Text.empty();
-        channelInfo.append(
-            Text.literal(data.displayName()).setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA)));
-        channelInfo.append(Text.literal("(").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY)));
-        channelInfo.append(Text.literal(data.login())
-            .setStyle(Style.EMPTY
-                .withColor(Formatting.DARK_AQUA)
-                .withItalic(true)));
-        channelInfo.append(Text.literal(")").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY)));
-        channelInfo.setStyle(Style.EMPTY
-            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                Text.empty()
-                    .append(Text.literal("クリックで"))
-                    .append(Text.literal("チャンネル"))
-                    .append(Text.literal("を開く"))))
-            .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, data.getUrl())));
-        response.append(channelInfo);
-
-        if (itr.hasNext()) {
-          response.append("\n");
-        }
-      }
-      if (result.cursor() != null) {
-        response.append("\n");
-        response.append("[");
-        response.append(Text.literal("次のページ").setStyle(
-            Style.EMPTY
-                .withClickEvent(
-                    new ClickEvent(ClickEvent.Action.RUN_COMMAND,
-                        "/twitch search " + query + " " + StringArgumentType.escapeIfRequired(
-                            result.cursor())))
-                .withUnderline(true)
-                .withBold(true)
-                .withItalic(true)
-                .withColor(Formatting.GRAY)));
-        response.append("]");
-      }
-      context.getSource().sendFeedback(response);
+      MutableText resultText = getSearchResultText(query, result);
+      context.getSource().sendFeedback(resultText);
     }));
     return 1;
+  }
+
+  private MutableText getSearchResultText(String query, SearchChannelsResult result) {
+    var response = Text.empty();
+    response.append(
+        Text.literal(query + "の検索結果 ==========\n")
+            .setStyle(Style.EMPTY
+                .withBold(true)
+                .withColor(Formatting.GOLD)));
+    var itr = result.channels().iterator();
+    while (itr.hasNext()) {
+      var data = itr.next();
+      var connect = Text.empty();
+      connect.append(Text.literal("["));
+      connect.append(Text.literal("接続")
+          .setStyle(Style.EMPTY
+              .withColor(Formatting.DARK_GREEN)
+              .withBold(true)));
+      connect.append(Text.literal("]"));
+      connect.setStyle(Style.EMPTY
+          .withHoverEvent(
+              new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                  Text.empty()
+                      .append(Text.literal("クリックして"))
+                      .append(Text.literal(data.displayName())
+                          .setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA).withItalic(true)))
+                      .append(Text.literal("のチャットに接続する"))))
+          .withClickEvent(
+              new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/twitch connect " + data.login())));
+      response.append(connect);
+      response.append(" ");
+      var channelInfo = Text.empty();
+      channelInfo.append(
+          Text.literal(data.displayName()).setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA)));
+      channelInfo.append(Text.literal("(").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY)));
+      channelInfo.append(Text.literal(data.login())
+          .setStyle(Style.EMPTY
+              .withColor(Formatting.DARK_AQUA)
+              .withItalic(true)));
+      channelInfo.append(Text.literal(")").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY)));
+      channelInfo.setStyle(Style.EMPTY
+          .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+              Text.empty()
+                  .append(Text.literal("クリックで"))
+                  .append(Text.literal("チャンネル"))
+                  .append(Text.literal("を開く"))))
+          .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, data.getUrl())));
+      response.append(channelInfo);
+
+      if (itr.hasNext()) {
+        response.append("\n");
+      }
+    }
+    if (result.cursor() != null) {
+      response.append("\n");
+      response.append("[");
+      response.append(Text.literal("次のページ").setStyle(
+          Style.EMPTY
+              .withClickEvent(
+                  new ClickEvent(ClickEvent.Action.RUN_COMMAND,
+                      "/twitch search " + query + " " + StringArgumentType.escapeIfRequired(
+                          result.cursor())))
+              .withUnderline(true)
+              .withBold(true)
+              .withItalic(true)
+              .withColor(Formatting.GRAY)));
+      response.append("]");
+    }
+    return response;
   }
 
   private int connectToDefault(CommandContext<FabricClientCommandSource> context) {
